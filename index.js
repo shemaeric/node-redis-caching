37550a1ecfa365a1fb2b4483fdf4351c45de4a32
shemaeric <shemaeric01@gmail.com>
2020-09-21 12:31:15 +0200

    commit all

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5001;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+
+function setResponse(username, repos) {
+    return `<h2>${username} has ${repos} Github repos</h2>`
+}
+
+// Get Repositories on Github
+async function getRepos(req, res, next) {
+    try {
+        
+        const { username } = req.params;
+        const response = await fetch(`https://api.github.com/users/${username}`)
+
+        const data = await response.json()
+
+        const repos = data.public_repos
+
+        client.setex(username, 3600, repos)
+
+        res.send(setResponse(username, repos))
+    } catch (error) {
+        console.log('error', error)
+        return res.status(500)
+    }
+}
+
+// cache middleware
+function cache(req, res, next) {
+    const { username } = req.params;
+
+    client.get(username, (error, data) => {
+        if(error) throw error;
+
+        if(data !== null) {
+            res.send(setResponse(username, data))
+        }
+        else {
+            next()
+        }
+    })
+}
+app.get('/repos/:username', cache, getRepos);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
